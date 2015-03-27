e10b4eae4d88fb19531aea2d150c620b02255f09
shiman <guoshim@gmail.com>
2015-03-27 13:38:40 -0400

    dependency tree data structure

--- dependency_tree.py
+++ dependency_tree.py
@@ -1,4 +1,3 @@
-#TODO fromstring() not working yet
 class DepTree(object):
     """
     >>> s = '''poss(dog-2, My-1)
@@ -9,17 +8,17 @@ class DepTree(object):
     ... dobj(eating-5, sausage-6)'''
     >>> tree = DepTree.fromstring(s)
     >>> print tree
-    (dog-2, My-1)
-    (likes-4, dog-2)
-    (likes-4, also-3)
-    (ROOT-0, likes-4)
-    (likes-4, eating-5)
-    (eating-5, sausage-6)
+    ROOT-0, likes-4
+    likes-4, dog-2
+    dog-2, My-1
+    likes-4, also-3
+    likes-4, eating-5
+    eating-5, sausage-6
     """
-    def __init__(self, token, children=[], parent=None, index=None):
+    def __init__(self, token, index=None):
         self.token = token
-        self._children = children
-        self._parent = parent
+        self._children = list()
+        self._parent = None
         self._index = index
 
     def is_root(self):
@@ -39,7 +38,7 @@ class DepTree(object):
 
     @property
     def parent(self):
-        return self.parent
+        return self._parent
 
     @parent.setter
     def parent(self, mother_tree):
@@ -59,30 +58,38 @@ class DepTree(object):
         self._children.append(subtree)
         subtree.parent = self
 
-    def __str__(self):
+    def _get_pairs(self):
         pairs = list()
         for c in self.children:
             parent = "{0:s}-{1:d}".format(self.token, self._index)
             child = "{0:s}-{1:d}".format(c.token, c._index)
             pairs.append(parent + ', ' + child)
-            pairs.append(self.__str__(c))
+            pairs += c._get_pairs()
+        return pairs
+
+    def __str__(self):
+        pairs = self._get_pairs()
         return '\n'.join(pairs)
 
     @classmethod
     def fromstring(cls, s):
+        """
+        Construct a DepTree from Stanford Parser format string
+        """
         leaves = dict()
         for line in s.strip().split('\n'):
             parent, child = line[line.find('(')+1: -1].split(', ')
             if parent not in leaves:
                 parent_token, parent_index = parent.split('-')
-                leaves[parent] = parent_node
                 parent_node = cls(parent_token, index=int(parent_index))
+                leaves[parent] = parent_node
             if child not in leaves:
                 child_token, child_index = child.split('-')
                 child_node = cls(child_token, index=int(child_index))
                 leaves[child] = child_node
             leaves[parent].add_child(leaves[child])
         return leaves['ROOT-0']
+
 
 if __name__ == '__main__':
     import doctest

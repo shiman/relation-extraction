92bcfecb3c54942fde84e10870a69ead4f302f1a
luutuntin <luutuntin@users.noreply.github.com>
2015-03-29 07:19:23 +0700

    Update features.py
    Add basic features for entity kernel.

--- features.py
+++ features.py
@@ -27,6 +27,39 @@ def between_words(mentionpair):
     right_index = mentionpair.right.indices[0]
     sentence_tokens = mentionpair.left.get_sentence_tokens(documents)
     return "BETWEEN_WORD={}".format('_'.join(sentence_tokens[left_index: right_index]))
+    
+# FEATURES - KERNEL - ENTITY
+def entity_type_e1(rel):
+    """
+    -> entity type of the first entity mention
+    """
+    return "type_e1=" + rel.antecedent.netype
+
+def entity_type_e2(rel):
+    """
+    -> entity type of the second entity mention
+    """
+    return "type_e2=" + rel.anaphor.netype
+
+def entity_token_dist(rel):
+    """
+    -> number of tokens between two mentions
+    """
+    token_dist = rel.anaphor.indices[0] - \
+                 rel.antecedent.indices[-1]
+    if token_dist<3:
+        return "token_dist=" + str(token_dist)
+    else:
+        return  "token_dist=n"
+
+# FEATURES - KERNEL - TREE - SYNTACTIC
+def syntactic_pt(rel):
+    """
+    -> number of common sub-trees of Path-enclosed Tree
+    """
+    pass
+
+# FEATURES - KERNEL _ TREE - DEPENDENCY
 
 # ##############################
 
